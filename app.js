e8040543e35c7c580033c85bae456f5b15c0ff7d
Undermove <undermove@yandex.ru>
2018-09-13 14:37:59 +0300

    Some changes

--- app.js
+++ app.js
@@ -26,107 +26,27 @@ new Vue({
     
     methods: {
         send: function () {
-            if (this.newMsg != '') {
-                var a = {
-                    type: "message",
-                    email: this.email,
-                    username: this.username,
-                    message: $('<p>').html(this.newMsg).text() // Strip out html
-                }
-                this.ws.send(
-                    JSON.stringify(a));
-                this.newMsg = ''; // Reset newMsg
-            }
+
         },
 
         createRoom: function () {
-            if (!this.newRoom) {
-                Materialize.toast('You must enter room name', 2000);
-                return
-            }
-            
-            var createRequest = {
-                roomName: this.newRoom
-            }
-            
-            var wrappedCreateRequest = {
-                type: "CreateRoomRequest",
-                raw: createRequest
-            }
 
-            this.ws.send(JSON.stringify(wrappedCreateRequest));
         },
 
         join: function () {
-            if (!this.username) {
-                Materialize.toast('You must choose a username', 2000);
-                return
-            }
 
-            var authRequest = {
-                username: this.username
-            }
-            
-            var wrappedAuthRequest = {
-                type: "AuthRequest",
-                raw: authRequest
-            }
-
-            this.ws.send(JSON.stringify(wrappedAuthRequest));
         },
 
         enterRoom:function (roomName){
-            if (!roomName) {
-                Materialize.toast('You must choose a room', 2000);
-                return
-            }
 
-            var enterRoomRequest = {
-                roomname: roomName
-            }
-            
-            var wrappedEnterRoomRequest = {
-                type: "EnterRoomRequest",
-                raw: enterRoomRequest
-            }
-
-            this.ws.send(JSON.stringify(wrappedEnterRoomRequest));
         },
 
         leaveRoom:function (){
-            if (!this.currentRoom) {
-                Materialize.toast('You are not in room', 2000);
-                return
-            }
 
-            var leaveRoomRequest = {
-                roomname: this.currentRoom
-            }
-            
-            var wrappedLeaveRoomRequest = {
-                type: "LeaveRoomRequest",
-                raw: leaveRoomRequest
-            }
-
-            this.ws.send(JSON.stringify(wrappedLeaveRoomRequest));
         },
 
         turn:function (playerChiose){
-            if (!this.currentRoom) {
-                Materialize.toast('You are not in room', 2000);
-                return
-            }
 
-            var turnRequest = {
-                choise: playerChiose
-            }
-            
-            var wrappedturnRequest = {
-                type: "TurnRequest",
-                raw: turnRequest
-            }
-
-            this.ws.send(JSON.stringify(wrappedturnRequest));
         }
     }
 });
